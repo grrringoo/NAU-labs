e2981e65d334362c5e7f7ae980c1658b4cc4e12c
grrringo <andriihalaiko9@gmail.com>
2023-02-21 19:14:52 +0200

    IT lab1 completed

---
+++ IT/lab1/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Product from "./components/Product.vue";
+</script>
+
+<template>
+  <div class="ProductList">
+    <Product
+      name="Темлі шкарпетки"
+      description="Теплі шкарпетки дуже теплі ну дуже круті неймовірні"
+      image="/src/assets/vmSocks-blue.png"
+    />
+    <Product
+      name="Темлі шкарпетки"
+      description="Теплі шкарпетки дуже теплі ну дуже круті неймовірні"
+      image="/src/assets/vmSocks-green.jpg"
+    />
+  </div>
+  <a class="Link" href="https://www.amazon.com/s?k=socks&ref=nb_sb_noss"
+    >Більше шкарпеток тут</a
+  >
+</template>
+
+<style>
+.ProductList {
+  display: flex;
+}
+
+.Link {
+  font-size: 28px;
+}
+</style>
---
+++ IT/lab1/src/components/Product.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps(["name", "description", "image"]);
+</script>
+
+<template>
+  <div class="product">
+    <div class="product-info">
+      <h1>{{ name }}</h1>
+      <p>{{ description }}</p>
+    </div>
+    <img class="product-image" :src="image" />
+  </div>
+</template>
+
+<style>
+.product-image {
+  max-width: 100%;
+  width: 540px;
+}
+</style>
